2dae4cf108a73aa2f3da52fa699580a8a4370aa6
Jay7221 <jns29072003@gmail.com>
2023-08-09 22:25:50 +0530

    Made changes for deployment on vercel

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,22 +1,26 @@
-import './WorkCard.css';
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import "./WorkCard.css";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const WorkCard = (props) => {
   console.log(props);
   return (
     <div className="project-card">
-        <img src={ props.prop.imgsrc } alt="Project" />
-        <h2 className="project-title">{ props.prop.title }</h2>
-        <div className="pro-details">
-            <p>{ props.prop.text }</p>
-            <div className="pro-btns">
-                <NavLink to={ props.prop.view } className="btn">VIEW</NavLink>
-                <NavLink to={ props.prop.source } className="btn">SOURCE</NavLink>
-            </div>
+      <img src={props.prop.imgsrc} alt="Project" />
+      <h2 className="project-title">{props.prop.title}</h2>
+      <div className="pro-details">
+        <p>{props.prop.text}</p>
+        <div className="pro-btns">
+          <NavLink to={props.prop.view} className="btn">
+            VIEW
+          </NavLink>
+          <NavLink to={props.prop.source} className="btn">
+            SOURCE
+          </NavLink>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkCard
+export default WorkCard;
