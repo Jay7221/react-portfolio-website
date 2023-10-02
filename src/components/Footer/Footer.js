5e657301dce9cdc4ccf6f0e3a1920eba084e8644
Jay7221 <jns29072003@gmail.com>
2023-10-02 17:33:17 +0530

    modified footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,59 @@
-import './index.css';
-import React from 'react'
-import { FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+// Footer.js
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faCodepen, // You can replace this with the appropriate Codechef icon
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faMapMarkerAlt,
+  faPhone,
+  faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
+import { socialHandles } from '../../constants/SocialHandles';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-container">
-        <div className="left">
-          <div className="location">
-            <FaHome size={20} style={{color: "#fff", marginRight:"2rem"}}/>
-            <div>
-              <p>Vishrambag, Sangli</p>
-              <p>India</p>
+    <footer className="bg-gray-900 text-white py-6">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:mb-0">
+            <h3 className="text-2xl font-semibold">Contact Information</h3>
+            <div className="flex items-center m-4">
+              <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2 text-2xl" />
+              <p className="text-lg">Pune, Maharashtra, India - 412105</p>
+            </div>
+            <div className="flex items-center m-4">
+              <FontAwesomeIcon icon={faPhone} className="mr-2 text-2xl" />
+              <p className="text-lg">+91 8390832186</p>
+            </div>
+            <div className="flex items-center m-4">
+              <FontAwesomeIcon icon={faEnvelope} className="mr-2 text-2xl" />
+              <p className="text-lg">jns29072003@gmail.com</p>
             </div>
           </div>
-          <div className="phone">
-            <h4>
-              <FaPhone size={20} style={{color: "#fff", marginRight:"2rem"}}/>
-              8390832186
-            </h4>
-          </div>
-          <div className="email">
-              <FaMailBulk size={20} style={{color: "#fff", marginRight:"2rem"}}/>
-              jns29072003@gmail.com
-          </div>
-        </div>
-        <div className="right">
-          <h4>About me</h4>
-          <p>This is me Jay Shirugpe.</p>
-          <div className="social">
-            <Link to="https://github.com/Jay7221">
-              <FaGithub size={30} style={{color: "#fff", marginRight:"2rem"}}/>
-            </Link>
-            <Link to="https://www.linkedin.com/in/jay-shirgupe-650ab7233/">
-              <FaLinkedin size={30} style={{color: "#fff", marginRight:"2rem"}}/>
-            </Link>
+          <div className="flex space-x-4">
+            {
+              socialHandles.map(
+                (handle) => (
+            <a
+              href={handle.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+
+              <handle.icon className="text-sm" />
+            </a>
+                )
+              )
+            }
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
