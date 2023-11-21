244de68c604120cb7cafc3437592b9c79efc7d45
Jay7221 <jns29072003@gmail.com>
2023-11-21 11:19:16 +0530

    Added componets

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, modi dolorem in, officia atque cupiditate amet minima a labore, natus nostrum nihil placeat. Aspernatur, veniam officia? Mollitia non quae nobis pariatur, est fuga suscipit maiores deleniti voluptatibus laborum fugit dolorum minima quas, eligendi iure at quibusdam officia quasi quis quos!</p>
+
+                <br />
+
+                <p className='text-xl mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, minus ipsum earum totam at sit temporibus architecto. Iusto nisi ratione dolor deserunt sunt consequuntur consequatur praesentium, nostrum neque aliquam tenetur harum minus laudantium, accusantium perferendis adipisci nemo possimus, numquam nulla dolorem deleniti beatae! Minus doloribus aperiam officiis nulla quos molestias?</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
